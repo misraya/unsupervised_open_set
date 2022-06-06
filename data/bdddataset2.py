5e02676bf3d7506b39e552ab69217539f13721ac
tanjary21 <79201814+tanjary21@users.noreply.github.com>
2022-06-07 02:14:10 +0300

    new bdd dataloader and augs
    Modified bdd100k dataset class to exclude augmentations. Created new augmentation class to handle augmentations AFTER regeneration.

---
+++ data/bdddataset2.py
@@ -0,0 +1,212 @@
+import torch
+from torch.utils.data import Dataset
+from pycocotools.coco import COCO
+from torchvision.io import read_image
+import cv2
+import albumentations as A
+import numpy as np
+
+class OpenSetBDDDataset(Dataset):
+    def __init__(self, split=0, trainval='train', imgDir='data/bdd/images', annDir='data/bdd/labels/det_20'):
+        self.trainval = trainval # can be 'train' or 'val', determines how annotations are filtered
+        
+        self.imgDir = f'{imgDir}/{trainval}/'
+        self.annDir = f'{annDir}/det_{trainval}_cocofmt.json'
+        self.coco = COCO(self.annDir)
+
+        self.ood_splits = [['bicycle', 'motorcycle', 'rider', 'train']]
+        self.closed_splits = [['pedestrian', 'car', 'truck', 'bus', 'traffic light', 'traffic sign']]
+
+        self.ood_categories = ['bicycle', 'motorcycle', 'rider', 'train'] # self.ood_splits[split] # [8, 7, 2, 6]
+        self.closed_categories = ['pedestrian', 'car', 'truck', 'bus', 'traffic light', 'traffic sign'] # self.closed_splits[split] # [1, 3, 4, 5, 9, 10]
+        self.full_categories = self.ood_categories + self.closed_categories
+        self.remap_dict, self.unmap_dict = self.remap_categories()
+
+        self.aug_labels_dict = self.build_augmentation_labels_dict()
+        self.imgIds = self.coco.getImgIds() # all images in dataset
+        
+        self.h_flip = A.Compose([A.HorizontalFlip(p=1.0)], bbox_params=A.BboxParams(format='coco', label_fields=['class_labels']))
+        self.v_flip = A.Compose([A.VerticalFlip(p=1.0)], bbox_params=A.BboxParams(format='coco', label_fields=['class_labels']))
+        self.rot_90 = A.Compose([A.augmentations.geometric.rotate.RandomRotate90()], bbox_params=A.BboxParams(format='coco', label_fields=['class_labels']))
+        
+    def numpy_2_ch_tensor(self, img_numpy):
+        return torch.permute(torch.Tensor(img_numpy),(2,0,1))
+        
+    def remap_categories(self):
+        # this is so that closed category ids such as [1, 3, 4, 5, 9, 10] get mapped to [0, 1, 2, 3, 4, 5]
+        # then the detector's classification head only needs 6 output neurons, and loss can be computed easily
+        remap_dict = {}
+        unmap_dict = {}
+        # To do: check what class 0 is supposed to mean for frcnn. background or...?
+        for i, catId in enumerate(self.coco.getCatIds(catNms=self.closed_categories)):
+            remap_dict[catId] = i + 1 #### IMPORTANT: save the 0 label for background
+            unmap_dict[i+1] = catId
+        print(f'Remapping category labels... \n {remap_dict}')
+        return remap_dict, unmap_dict
+    
+    def build_augmentation_labels_dict(self):
+        aug_labels_dict = {'111': 0, # (v_flipped == 1 and h_flipped == 1 and factor == 1)
+                           '003': 0, # (v_flipped == 0 and h_flipped == 0 and factor == 3)
+                           '112': 1, # (v_flipped == 1 and h_flipped == 1 and factor == 2)
+                           '000': 1, # (v_flipped == 0 and h_flipped == 0 and factor == 0)
+                           '113': 2, # (v_flipped == 1 and h_flipped == 1 and factor == 3)
+                           '001': 2, # (v_flipped == 0 and h_flipped == 0 and factor == 1)
+                           '110': 3, # (v_flipped == 1 and h_flipped == 1 and factor == 0)
+                           '002': 3, # (v_flipped == 0 and h_flipped == 0 and factor == 2)
+                           '101': 4, # (v_flipped == 1 and h_flipped == 0 and factor == 1)
+                           '013': 4, # (v_flipped == 0 and h_flipped == 1 and factor == 3)
+                           '102': 5, # (v_flipped == 1 and h_flipped == 0 and factor == 2)
+                           '010': 5, # (v_flipped == 0 and h_flipped == 1 and factor == 0)
+                           '103': 6, # (v_flipped == 1 and h_flipped == 0 and factor == 3)
+                           '011': 6, # (v_flipped == 0 and h_flipped == 1 and factor == 1)
+                           '100': 7, # (v_flipped == 1 and h_flipped == 0 and factor == 0)
+                           '012': 7  # (v_flipped == 0 and h_flipped == 1 and factor == 2)
+                          }
+        return aug_labels_dict
+        
+        
+    def __len__(self):
+        return len(self.imgIds)
+    
+    def __getitem__(self, idx):
+        imgId = self.imgIds[idx]
+        
+        imgPath = f'{self.imgDir}/{self.coco.loadImgs([imgId])[0]["file_name"]}'
+        #img_tensor = read_image(imgPath) # 3 x 720(H) x 1280(W)
+        img_numpy = cv2.imread(imgPath) # 720(H) x 1280(W) x 3
+        img_numpy = cv2.cvtColor(img_numpy, cv2.COLOR_BGR2RGB)/255
+        #img_numpy = torch.permute( img_tensor.clone(), (1,2,0) ).numpy() # keep a copy in numpy formate for augmenting
+        
+        annIds = self.coco.getAnnIds(imgIds=imgId, iscrowd=None)
+        anns = self.coco.loadAnns(annIds)
+        anns = self.remove_ood_annotations(anns)
+        anns = self.prepare_anns_frcnn_fmt(anns)
+        
+        #img_aug, anns_aug = self.augment(img_numpy.copy(), anns)
+        
+        img_tensor = self.numpy_2_ch_tensor(img_numpy)
+        #img_aug_tensor = self.numpy_2_ch_tensor(img_aug)
+        anns = self.convert_xywh_2_x1y1x2y2(anns)
+        #anns_aug = self.convert_xywh_2_x1y1x2y2(anns_aug)
+        return img_numpy, img_tensor, anns#, img_aug_tensor, anns_aug
+    
+    def convert_xywh_2_x1y1x2y2(self, anns):
+        bboxes = anns['boxes']
+        bboxes[:,2] = bboxes[:,2] + bboxes[:,0]
+        bboxes[:,3] = bboxes[:,3] + bboxes[:,1]
+        anns['boxes'] = bboxes
+        return anns
+    
+    def remove_ood_annotations(self, anns):
+        ood_ids = self.coco.getCatIds(catNms=self.ood_categories)
+        new_anns = []
+        for ann in anns:
+            if ann['category_id'] in ood_ids:
+                if self.trainval == 'val':
+                    ann['category_id'] = 0 ## IMPORTANT: This sets the class as background so that detector can compute loss
+                    new_anns.append(ann)
+            else:
+                new_anns.append(ann)
+        return new_anns
+
+    def prepare_anns_frcnn_fmt(self, anns):
+        gt_bboxes = []
+        gt_classes = []
+        gt_augs = []
+        for ann in anns:
+            gt_bboxes.append(ann['bbox'])
+            gt_classes.append(self.remap_dict[ann['category_id']])
+
+        new_anns = {'boxes': torch.Tensor(gt_bboxes),
+                    'labels': torch.Tensor(gt_classes).long(),
+                    'aug_labels': torch.ones(len(gt_classes)).long() # the label 1 represents no augmentation via '112'
+                   }
+        return new_anns
+    
+    def augment(self, img_numpy, anns):
+        bboxes = anns['boxes'].clone().numpy()
+        labels = anns['labels'].clone().numpy()
+        
+        # apply horizontal_flip
+        if np.random.rand() > 0.5:
+            transformed = self.h_flip(image=img_numpy, 
+                                      bboxes=bboxes, 
+                                      class_labels=labels )
+            img_numpy = transformed['image']
+            bboxes = np.array([list(bbox) for bbox in transformed['bboxes']])
+            labels = transformed['class_labels']
+            
+            h_flipped = 1
+        else:
+            h_flipped = 0
+            
+        # apply vertical_flip
+        if np.random.rand() > 0.5:
+            transformed = self.v_flip(image=img_numpy, 
+                                      bboxes=bboxes, 
+                                      class_labels=labels)
+            img_numpy = transformed['image']
+            bboxes = np.array([list(bbox) for bbox in transformed['bboxes']])
+            labels = transformed['class_labels']
+            
+            v_flipped = 1
+        else:
+            v_flipped = 0
+            
+        # apply random rotate
+        rot_factor = np.random.rand()
+        if rot_factor < 0.25:
+            factor = 0
+        elif rot_factor < 0.5:
+            factor = 1
+        elif rot_factor < 0.75:
+            factor = 2
+        elif rot_factor < 1.0:
+            factor = 3
+            
+        transformed = self.rot_90(image=img_numpy, 
+                                  bboxes=bboxes, 
+                                  class_labels=labels, 
+                                  factor=factor)
+        img_numpy = transformed['image']
+        bboxes = np.array([list(bbox) for bbox in transformed['bboxes']])
+        labels = transformed['class_labels']
+        
+        # determine augmentation label
+        aug_key = str(v_flipped) + str(h_flipped) + str(factor)
+        aug_label = self.aug_labels_dict[aug_key]
+        
+        
+        # apply label to annotations for self-supervised branch
+        new_anns = {'boxes': torch.Tensor(bboxes),
+                    'labels': torch.Tensor(labels).long(),
+                    'aug_labels': (aug_label*torch.ones(len(labels))).long()
+                   }
+        
+        return img_numpy, new_anns
+        
+        
+def myCollate(data):
+    img_numpys, img_tensors, annss = [], [], []
+    for sample in data:
+        img_numpys.append(sample[0])
+        img_tensors.append(sample[1])
+        annss.append(sample[2])
+    return img_numpys, img_tensors, annss
+
+def move_to_cuda(main_list): #[img_tensors, annss, img_tensor_augs, anns_augs]
+    new_main_list = []
+    for batch_item in main_list:
+        if type(batch_item[0]) == dict:
+            new_batch_item = []
+            for dict_item in batch_item:
+                new_dict_item = {}
+                for key, value in dict_item.items():
+                    new_dict_item[key] = value.cuda()
+                new_batch_item.append(new_dict_item)
+            new_main_list.append(new_batch_item)
+        elif type(batch_item[0]) == torch.Tensor:
+            new_main_list.append([img_tensor.cuda() for img_tensor in batch_item])
+    return new_main_list
+        
+    
---
+++ data/customAugmentor.py
@@ -0,0 +1,112 @@
+import torchvision.transforms as T
+import albumentations as A
+import numpy as np
+import torch
+
+class CustomAugmentor():
+    def __init__(self):
+        
+
+        a0 = A.Compose([A.VerticalFlip(p=1), 
+                        A.HorizontalFlip(p=1), 
+                        A.Affine(rotate=90, fit_output=True, p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # factor 1 
+        a1 = A.Compose([A.VerticalFlip(p=1), 
+                        A.HorizontalFlip(p=1), 
+                        A.Affine(rotate=180, fit_output=True, p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # factor 2
+        a2 = A.Compose([A.VerticalFlip(p=1), 
+                        A.HorizontalFlip(p=1), 
+                        A.Affine(rotate=270, fit_output=True, p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # factor 3
+        a3 = A.Compose([A.VerticalFlip(p=1), 
+                        A.HorizontalFlip(p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # factor 0
+        a4 = A.Compose([A.VerticalFlip(p=1), 
+                        A.Affine(rotate=90, fit_output=True, p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # factor 1
+        a5 = A.Compose([A.VerticalFlip(p=1), 
+                        A.Affine(rotate=180, fit_output=True, p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # factor 2
+        a6 = A.Compose([A.VerticalFlip(p=1), 
+                        A.Affine(rotate=270, fit_output=True, p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # factor 3
+        a7 = A.Compose([A.VerticalFlip(p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # factor 0
+        a_ = A.Compose([A.Affine(rotate=90, fit_output=True, p=1)], 
+                       bbox_params=A.BboxParams(format='pascal_voc', 
+                                                label_fields=['class_labels'])) # test
+
+
+
+        t0 = T.Compose([T.RandomVerticalFlip(p=1.), 
+                        T.RandomHorizontalFlip(p=1.), 
+                        T.RandomRotation(degrees=[270,270], 
+                                         expand=True)]) # factor 1
+        t1 = T.Compose([T.RandomVerticalFlip(p=1.), 
+                        T.RandomHorizontalFlip(p=1.), 
+                        T.RandomRotation(degrees=[180,180], 
+                                         expand=True)]) # factor 2
+        t2 = T.Compose([T.RandomVerticalFlip(p=1.), 
+                        T.RandomHorizontalFlip(p=1.), 
+                        T.RandomRotation(degrees=[90,90], 
+                                         expand=True)]) # factor 3
+        t3 = T.Compose([T.RandomVerticalFlip(p=1.), 
+                        T.RandomHorizontalFlip(p=1.)]) # factor 0
+        t4 = T.Compose([T.RandomVerticalFlip(p=1.), 
+                        T.RandomRotation(degrees=[270,270], 
+                                         expand=True)]) # factor 1
+        t5 = T.Compose([T.RandomVerticalFlip(p=1.), 
+                        T.RandomRotation(degrees=[180,180], 
+                                         expand=True)]) # factor 2
+        t6 = T.Compose([T.RandomVerticalFlip(p=1.), 
+                        T.RandomRotation(degrees=[90,90], 
+                                         expand=True)]) # factor 3
+        t7 = T.Compose([T.RandomVerticalFlip(p=1.)]) # factor 0
+        t_ = T.Compose([T.RandomRotation(degrees=[270,270], 
+                                         expand=True)])
+
+        #self.aug_factors = {0: 1, 1: 2, 2: 3, 3: 0, 4: 1, 5: 2, 6: 3, 7: 0}
+        self.at = [a0, a1, a2, a3, a4, a5, a6, a7, a_]
+        self.tt = [t0, t1, t2, t3, t4, t5, t6, t7, t_]
+        
+    def augment_annotations(self, img_numpys, annss, trans, index,):
+        new_img_numpys, new_anns = [], []
+        for img_numpy, ann in zip(img_numpys, annss):
+            transformed = trans(image=img_numpy, 
+                                bboxes=ann['boxes'].clone().cpu().numpy(),
+                                class_labels=ann['labels'].clone().cpu().numpy())
+            new_img_numpys.append(transformed['image'])
+            bboxes = torch.Tensor([list(bbox) for bbox in transformed['bboxes']]).cuda()
+            labels = torch.Tensor(transformed['class_labels']).long().cuda()
+
+            new_anns.append({'boxes': bboxes,
+                             'labels': labels,
+                             'aug_labels': index*ann['aug_labels'].clone()})
+
+        return new_img_numpys, new_anns
+    
+    def augment(self, img_numpys, img_tensors, reg_tensors, annss):
+        aug_i = np.random.randint(8) # select random [0,7] augmenttaion labels(also the index)
+        #aug_factor = self.aug_factors[aug_i] # find corresponding rotation factor for albumentations
+        #print(f'aug i/label: {aug_i}, aug factor: {aug_factor}')
+
+        t_i = self.tt[aug_i] # get appropriate torchvision transform function
+        a_i = self.at[aug_i] # get appropriate albumentation transform function
+
+        img_tensors_aug = [t_i(img_tensor.clone()) for img_tensor in img_tensors] # transform all img_tensors in list-batch
+        reg_tensors_aug = [t_i(reg_tensor.clone()) for reg_tensor in reg_tensors] # transform all regenerated img_tensors in list-batch
+        img_numpys_aug, anns_aug = self.augment_annotations(img_numpys.copy(), annss.copy(), a_i, aug_i)
+        
+        return img_numpys_aug, img_tensors_aug, reg_tensors_aug, anns_aug
+    
+    
+    
